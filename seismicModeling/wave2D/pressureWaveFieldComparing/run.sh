845d48bea719638f6f76d750b1a2a96fd0618776
Paulo Bastos <pbastos@id.uff.br>
2021-04-04 11:36:54 -0300

    Funcionando em sequência perfeitamente

--- seismicModeling/wave2D/pressureWaveFieldComparing/run.sh
+++ seismicModeling/wave2D/pressureWaveFieldComparing/run.sh
@@ -38,14 +38,14 @@ parFileName="parameters/modelingParameters.txt"
 echo -e "$nx\n$nz\n$nt\n$dx\n$dz\n$dt\n$nsrc\n$xsrc\n$zsrc\n$zrec" > $parFileName
 echo "Modeling parameters was built..."
 
-# pgcc -acc -fast -ta=tesla,cc60 basicModelingCodes/basicAcoustic2D.c -lm -o run.exe
-# ./run.exe $parFileName $simpGridRicker
+pgcc -acc -fast -ta=tesla,cc60 basicModelingCodes/basicAcoustic2D.c -lm -o run.exe
+./run.exe $parFileName $simpGridRicker
 
-# pgcc -acc -fast -ta=tesla,cc60 basicModelingCodes/basicAcousticVec2D.c -lm -o run.exe
-# ./run.exe $parFileName $stagGridRicker
+pgcc -acc -fast -ta=tesla,cc60 basicModelingCodes/basicAcousticVec2D.c -lm -o run.exe
+./run.exe $parFileName $stagGridRicker
 
-# pgcc -acc -fast -ta=tesla,cc60 basicModelingCodes/basicElasticIsotropic2D.c -lm -o run.exe
-# ./run.exe $parFileName $stagGridRicker
+pgcc -acc -fast -ta=tesla,cc60 basicModelingCodes/basicElasticIsotropic2D.c -lm -o run.exe
+./run.exe $parFileName $stagGridRicker
 
 timeCut=1200
 python3 comparingCode/compareSeismograms.py $nx $dx $nt $dt $timeCut $nsrc
